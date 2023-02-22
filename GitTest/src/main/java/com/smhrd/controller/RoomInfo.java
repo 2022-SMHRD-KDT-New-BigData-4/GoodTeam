5301ca12a903a57553e095049566746204271902
smhrd <smhrd@DESKTOP-1JRSK37>
2023-02-22 12:22:34 +0900

    111

---
+++ GitTest/src/main/java/com/smhrd/controller/RoomInfo.java
@@ -0,0 +1,19 @@
+package com.smhrd.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.command.Command;
+
+public class RoomInfo implements Command {
+
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws UnsupportedEncodingException {
+		// 객실 변호를 가져옴
+			//
+		return null;
+	}
+
+}
